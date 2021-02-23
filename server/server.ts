cf980ead8cad4ae21c5e7a1bf85eee31d589a164
polatengin <enpolat@microsoft.com>
2021-02-23 12:40:06 +0300

    server created

--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,10 @@
 import express from "express";
 import cors from "cors";
+import { createServer } from "http";
 
 const app = express();
 
 app.use(cors(corsOptions));
+
+const server = createServer(app);
+
