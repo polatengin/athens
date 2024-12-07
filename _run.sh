80584d5146c4244601cc5042a6380cea69a286cd
polatengin <enpolat@microsoft.com>
2024-12-07 17:35:58 +0000

    adding container build commands to publish client and server projects to acr

--- _run.sh
+++ _run.sh
@@ -1,5 +1,7 @@
 PROJECT_SUFFIX=$(head /dev/urandom | tr -dc a-z0-9 | head -c 5)
 
+TIMESTAMP=$(date +"%Y%m%d%H%M%S")
+
 az group create --name "rg-${PROJECT_SUFFIX}" --location "westus2"
 
 az acr create --resource-group "rg-${PROJECT_SUFFIX}" --name "acr${PROJECT_SUFFIX}" --sku "Basic"
@@ -7,3 +9,7 @@ az acr create --resource-group "rg-${PROJECT_SUFFIX}" --name "acr${PROJECT_SUFFI
 az aks create --resource-group "rg-${PROJECT_SUFFIX}" --name "aks-${PROJECT_SUFFIX}" --node-vm-size "Standard_D4_v5" --node-count "1" --attach-acr "acr${PROJECT_SUFFIX}" --generate-ssh-keys
 
 az acr login --name "acr${PROJECT_SUFFIX}"
+
+az acr build --registry "acr${PROJECT_SUFFIX}" --image "server:${TIMESTAMP}" ./server
+
+az acr build --registry "acr${PROJECT_SUFFIX}" --image "client:${TIMESTAMP}" ./client
