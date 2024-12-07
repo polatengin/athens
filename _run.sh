e1cbef92af33a14b361f22c6a1be5ad078554729
polatengin <enpolat@microsoft.com>
2024-12-07 21:57:49 +0000

    adding external ip address logged to terminal

--- _run.sh
+++ _run.sh
@@ -25,3 +25,5 @@ kubectl apply -f ./iac/ingress.yaml
 sed "s/{IMAGE}/${ACR_SERVER}\/server:${TIMESTAMP}/g" ./server/deploy.yml | kubectl apply -f -
 
 sed "s/{IMAGE}/${ACR_SERVER}\/client:${TIMESTAMP}/g" ./client/deploy.yml | kubectl apply -f -
+
+echo "http://$(kubectl get ingress -o jsonpath="{.items[0].status.loadBalancer.ingress[0].ip}")"
